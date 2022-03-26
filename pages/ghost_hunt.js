343e105475eba0be97f760dbf9a32d63a558e13d
RosaryDarling <ruta705@gmail.com>
2022-03-26 15:10:26 +0200

    section

--- pages/ghost_hunt.js
+++ pages/ghost_hunt.js
@@ -6,11 +6,12 @@ import logo from '/public/images/RD_.png'
 import darling from '/public/images/darling.png'
 import fusion from '/public/images/city_fusion.png'
 import hunt from '/public/images/ghost_hunt.png'
+import separator_1 from '/public/images/separator1.png'
 
 export default function Ghost_hunt() {
     return (
         <div className={styles.container}>
-            <main className={styles.main}>
+            <main className={styles.main_section}>
                 <div className={styles.section_divider}>
                     <Image
                         src={hunt}
@@ -19,8 +20,18 @@ export default function Ghost_hunt() {
                         height={30}
                         width={25}
                     ></Image>
-                    <hr className={styles.section_divider_line}></hr>
+                    <div className={styles.section_divider_line}>
+                        <Image
+                            src={separator_1}
+                            alt='Ghost Hunt'
+                            layout='responsive'
+                            height={2}
+                            width={900}
+                        ></Image>
+                    </div>
                 </div>
+
+
 
             </main>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -320,14 +320,9 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
+        <a>
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/vercel.svg" alt="Footer" width={72} height={16} />
           </span>
         </a>
       </footer>
