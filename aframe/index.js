b76d06c58bdfff013ffae15e27c98a5531cea7d1
RosaryDarling <ruta705@gmail.com>
2022-04-05 21:59:48 +0300

    saving tutorial progress

--- aframe/index.js
+++ aframe/index.js
@@ -1,42 +1,42 @@
 import * as THREE from 'https://unpkg.com/three@0.139.2/build/three.module.js';
   
 const scene = new THREE.Scene();
-console.log(scene)
+const camera = new THREE.PerspectiveCamera(
+	75, innerWidth / innerHeight, 0.1, 1000
+)
+const renderer = new THREE.WebGLRenderer()
 
 
+renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(devicePixelRatio)
+
+document.body.appendChild(renderer.domElement)
 
 
+// const BoxGeometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({
+// 	color:0x00FF00
+// })
+// const mesh = new THREE.Mesh(BoxGeometry, material)
+// scene.add(mesh)
+camera.position.z = 5
+
+const PlaneGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
+const planeMaterial = new THREE.MeshBasicMaterial({
+	color:0xFF0000,
+	side: THREE.DoubleSide
+})
+const planeMesh = new THREE.Mesh(PlaneGeometry, planeMaterial)
+scene.add(planeMesh)
+
+function animate () {
+	requestAnimationFrame(animate)
+	renderer.render(scene, camera)
+	// mesh.rotation.x += 0.01
+	// mesh.rotation.y += 0.01
+	// planeMesh.rotation.x += 0.01
+}
+
+animate()
 
 
-
-
-
-
-
-
-
-
-
-
-
-// import * as THREE from 'three';
-
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight);
-// document.body.appendChild( renderer.domElement);
-
-// const geometry = new THREE.BoxGeometry(2, 2, 2);
-// const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
-// camera.position.z = 5;
-
-// function animate() {
-// 	requestAnimationFrame( animate );
-// 	renderer.render( scene, camera );
-// }
-// animate();
